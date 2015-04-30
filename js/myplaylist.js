e9fd9f664bdc6c9720526b295cbc9bbc6501072f
Chris Luk <luk@chrisluk.im>
2015-04-30 15:39:56 -0400

    working files without songs/artwork

---
+++ js/myplaylist.js
@@ -0,0 +1,262 @@
+/**
+ * Created by chrisluk
+ * User: Chris Luk
+ * Date: 3/5/15
+ * Time: 1:04 PM
+ */
+
+var myPlaylist = [
+    {
+        mp3:'mix/07 See You Again (feat. Charlie Puth).mp3',
+        oga:'',
+        title:'See You Again',
+        artist:'Wiz Khalifa feat. Charlie Puth',
+        rating:5,
+        buy:'mix/07 See You Again (feat. Charlie Puth).mp3',
+        price:'Download',
+        duration:'3:50',
+        cover:'mix/sya.png'
+    },
+    {
+        mp3:'mix/Taylor Swift - 22.mp3',
+        oga:'',
+        title:'22',
+        artist:'Taylor Swift',
+        rating:5,
+        buy:'mix/Taylor Swift - 22.mp3',
+        price:'Download',
+        duration:'3:52',
+        cover:'mix/22.png'
+    },
+    {
+        mp3:'mix/Austin Mahone - Someone Like You.mp3',
+        oga:'',
+        title:'Someone Like You',
+        artist:'Austin Mahone',
+        rating:5,
+        buy:'mix/Austin Mahone - Someone Like You.mp3',
+        price:'Download',
+        duration:'5:06',
+        cover:'mix/sly.jpg'
+    },
+    {
+        mp3:'mix/Meghan Trainor - Dear Future Husband.mp3',
+        oga:'',
+        title:'Dear Future Husband',
+        artist:'Meghan Trainor',
+        rating:5,
+        buy:'mix/Meghan Trainor - Dear Future Husband.mp3',
+        price:'Download',
+        duration:'3:07',
+        cover:'mix/mt.png'
+    },
+    {
+        mp3:'mix/Vanic X machineheart - Circles.mp3',
+        oga:'',
+        title:'Circles',
+        artist:'Vanic X Machineheart',
+        rating:5,
+        buy:'mix/Vanic X machineheart - Circles.mp3',
+        price:'Download',
+        duration:'4:43',
+        cover:'mix/circles.jpg'
+    },
+    {
+        mp3:'mix/06 Best Mistake (feat. Big Sean).mp3',
+        oga:'',
+        title:'Best Mistake',
+        artist:'Ariana Grande feat. Big Sean',
+        rating:5,
+        buy:'mix/06 Best Mistake (feat. Big Sean).mp3',
+        price:'Download',
+        duration:'3:54',
+        cover:'mix/bm.png'
+    },
+    {
+        mp3:'mix/Snoop Dogg and Wiz Khalifa - Young, Wild and Free.mp3',
+        oga:'',
+        title:'Young, Wild and Free',
+        artist:'Snoop Dogg and Wiz Khalifa',
+        rating:5,
+        buy:'mix/Snoop Dogg and Wiz Khalifa - Young, Wild and Free.mp3',
+        price:'Download',
+        duration:'3:11',
+        cover:'mix/ywf.png'
+    },
+    {
+        mp3:'mix/Martin Garrix - Don\'t Look Down (feat. Usher).mp3',
+        oga:'',
+        title:'Don\'t Look Down',
+        artist:'Martin Garrix feat. Usher',
+        rating:5,
+        buy:'mix/Martin Garrix - Don\'t Look Down (feat. Usher).mp3',
+        price:'Download',
+        duration:'3:43',
+        cover:'mix/dld.png'
+    },
+    {
+        mp3:'mix/11 Save Rock and Roll (feat Elton John).mp3',
+        oga:'',
+        title:'Save Rock and Roll',
+        artist:'Fall Out Boy feat. Elton John',
+        rating:5,
+        buy:'mix/11 Save Rock and Roll (feat Elton John).mp3',
+        price:'Download',
+        duration:'4:41',
+        cover:'mix/srar.jpg'
+    },
+    {
+        mp3:'mix/Blackbear - 90210 feat. G-Eazy.mp3',
+        oga:'',
+        title:'90210',
+        artist:'Blackbear feat. G-Eazy',
+        rating:5,
+        buy:'mix/Blackbear - 90210 feat. G-Eazy.mp3',
+        price:'Download',
+        duration:'3:43',
+        cover:'mix/idfc.png'
+    },
+    {
+        mp3:'mix/Donnie Trumpet & The Social Experiment - Sunday Candy.mp3',
+        oga:'',
+        title:'Sunday Candy',
+        artist:'Donnie Trumpet & The Social Experiment',
+        rating:5,
+        buy:'mix/Donnie Trumpet & The Social Experiment - Sunday Candy.mp3',
+        price:'Download',
+        duration:'3:48',
+        cover:'mix/sc.png'
+    }, 
+    {
+        mp3:'mix/Girls Generation - Run Devil Run.mp3',
+        oga:'',
+        title:'Red Devil Run',
+        artist:'Girls Generation',
+        rating:5,
+        buy:'mix/Girls Generation - Run Devil Run.mp3',
+        price:'Download',
+        duration:'3:28',
+        cover:'mix/rdr.jpg'
+    },
+    {
+        mp3:'mix/Super Junior - It\'s You.mp3',
+        oga:'',
+        title:'It\'s You (&#45320;&#46972;&#44256;)',
+        artist:'Super Junior',
+        rating:5,
+        buy:'mix/Super Junior - It\'s You.mp3',
+        price:'Download',
+        duration:'3:51',
+        cover:'mix/sj.png'
+    },
+    {
+        mp3:'mix/I Don\'t Mind - Usher.mp3',
+        oga:'',
+        title:'I Don&#39;t Mind',
+        artist:'Usher feat. Juicy J',
+        rating:5,
+        buy:'mix/I Don\'t Mind - Usher.mp3',
+        price:'Download',
+        duration:'4:12',
+        cover:'mix/4.png'
+    },
+    {
+        mp3:'mix/01 Waiting for this Love.mp3',
+        oga:'',
+        title:'Waiting For This Love',
+        artist:'Austin Mahone',
+        rating:5,
+        buy:'mix/01 Waiting for this Love.mp3',
+        price:'Download',
+        duration:'3:49',
+        cover:'mix/wftl.png'
+    },
+    {
+        mp3:'mix/Omarion ft. Chris Brown & Jhene Aiko - Post To Be.mp3',
+        oga:'',
+        title:'Post To Be',
+        artist:'Omarion feat. Chris Brown & Jhene Aiko',
+        rating:5,
+        buy:'mix/0Omarion ft. Chris Brown & Jhene Aiko - Post To Be.mp3',
+        price:'Download',
+        duration:'3:49',
+        cover:'mix/ptb.png'
+    },
+    {
+        mp3:'mix/I\'ll Be Good.mp3',
+        oga:'',
+        title:'I\'ll Be Good',
+        artist:'Jaymes Young',
+        rating:5,
+        buy:'mix/I\'ll Be Good.mp3',
+        price:'Download',
+        duration:'4:10',
+        cover:'mix/ibg.png'
+    },
+    {
+        mp3:'mix/Snow Patrol - Chasing Cars.mp3',
+        oga:'',
+        title:'Chasing Cars',
+        artist:'Snow Patrol',
+        rating:5,
+        buy:'mix/Snow Patrol - Chasing Cars.mp3',
+        price:'Download',
+        duration:'4:25',
+        cover:'mix/cc.png'
+    },
+    {
+        mp3:'mix/Perfect Two - Auburn.mp3',
+        oga:'',
+        title:'Perfect Two',
+        artist:'Auburn',
+        rating:5,
+        buy:'mix/Perfect Two - Auburn.mp3',
+        price:'Download',
+        duration:'3:54',
+        cover:'mix/p2.png'
+    },
+    {
+        mp3:'mix/Like I\'m Gonna Lose You Ft. John Legend.mp3',
+        oga:'',
+        title:'Like I\'m Gonna Lose You',
+        artist:'Meghan Trainor feat. John Legend',
+        rating:5,
+        buy:'mix/Like I\'m Gonna Lose You Ft. John Legend.mp3',
+        price:'Download',
+        duration:'3:10',
+        cover:'mix/mt.png'
+    },
+    {
+        mp3:'mix/Two Is Better Than One (feat. Taylor Swift) - Boys Like Girls.mp3',
+        oga:'',
+        title:'Two Is Better Than One',
+        artist:'Boys Like Girls feat. Taylor Swift',
+        rating:5,
+        buy:'mix/Two Is Better Than One (feat. Taylor Swift) - Boys Like Girls.mp3',
+        price:'Download',
+        duration:'4:03',
+        cover:'mix/blg.png'
+    },
+    {
+        mp3:'mix/Goodbye - Who is Fancy.mp3',
+        oga:'',
+        title:'Goodbye',
+        artist:'Who Is Fancy?',
+        rating:5,
+        buy:'mix/Goodbye - Who is Fancy.mp3',
+        price:'Download',
+        duration:'3:27',
+        cover:'mix/2.png'
+    },
+    {
+        mp3:'mix/Sam Smith - Not In That Way.mp3',
+        oga:'',
+        title:'Not In That Way',
+        artist:'Sam Smith',
+        rating:5,
+        buy:'mix/Sam Smith - Not In That Way.mp3',
+        price:'Download',
+        duration:'2:52',
+        cover:'mix/nitw.png'
+    }
+];
